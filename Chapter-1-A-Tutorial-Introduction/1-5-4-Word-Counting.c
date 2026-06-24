39a67555d5a3c636de8ce8d2fb60fe721b97b7bc
Penguin <satish.karki94@gmail.com>
2026-06-23 23:53:03 -0400

    Added the topic 1-6-Array

--- Chapter-1-A-Tutorial-Introduction/1-5-4-Word-Counting.c
+++ Chapter-1-A-Tutorial-Introduction/1-5-4-Word-Counting.c
@@ -1,1 +1,44 @@
+
+/*BAre bone version of UNIX program wc*/
 #include <stdio.h>
+#define IN 1 /*Inside a word*/
+#define OUT 0 /*Outside a word*/
+
+/*
+int main(){
+    int c, nc, nl, nw, state;
+    state=OUT;
+    while((c=getchar())!= EOF){
+        ++nc;
+        if (c=='\n') 
+            ++nl;
+        if (c==' ' || c=='\t' || c=='\n')
+            state=OUT;
+        else if (state ==OUT){
+            state=IN;
+            ++nw;
+        }   
+    }
+    printf("The number of characters is %d,\nThe number of new line is %d,\nAnd the number of words is %d\n",nc,nl,nw);
+}
+    */
+
+
+    /*Exercise 1-12 - Write a program that prints its input one word per line*/
+int main(){
+    int c, state;
+    state=OUT;
+    while((c=getchar())!=EOF){
+        if (c==' '||c=='\n'||c=='\t'){
+            if(state==IN)
+                putchar('\n');
+            state=OUT;
+
+        }
+        else{
+            putchar(c);
+            state=IN;
+        }
+        
+    }
+}
